d24eb364128fbe3e3563c0d4a93add1f046d5167
ombran <ombran@gmail.com>
2014-10-05 18:10:09 +0900

    modify for Emacs24 package.el

--- init.el
+++ init.el
@@ -15,3 +15,8 @@
 (add-to-load-path "elisp")
 
 (require 'general)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
